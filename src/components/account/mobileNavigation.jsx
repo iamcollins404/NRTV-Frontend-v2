5ec38687e9eccd27fd6abc97467939b60beb9f29
iamcollins404 <iamcollins404dev@gmail.com>
2023-03-28 18:12:15 +0200

    Update mobileNavigation.jsx

--- src/components/account/mobileNavigation.jsx
+++ src/components/account/mobileNavigation.jsx
@@ -9,7 +9,7 @@ function MobileNavigation() {
                 <section id="bottom-navigation" class="md:hidden block fixed inset-x-0 bottom-0 z-10 bg-black border-t border-indigo-500 shadow">
                     <div id="tabs" class="flex justify-between py-2">
 
-                        <div className="items-center ml-10">
+                        <div className="items-center ml-6">
                             <a href="#" class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1" onClick={() => { navigate("/account/watch/live") }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white items-center w-6 h-6 mx-auto">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
@@ -42,7 +42,7 @@ function MobileNavigation() {
                             </a>
                         </div>
 
-                        <div className="items-center mr-10">
+                        <div className="items-center mr-6">
                             <a href="#" class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1" onClick={() => { navigate("/account/watch/live") }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white mx-auto">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
