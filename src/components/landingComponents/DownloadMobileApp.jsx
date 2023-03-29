8bd1e295d4d62fc3b240eeb36766baa46e1d57af
iamcollins404 <iamcollins404dev@gmail.com>
2023-03-29 09:16:23 +0200

    Update DownloadMobileApp.jsx

--- src/components/landingComponents/DownloadMobileApp.jsx
+++ src/components/landingComponents/DownloadMobileApp.jsx
@@ -113,7 +113,7 @@ function DownloadMobileApp() {
                             Download for Apple Ios
                         </a>
 
-                        <a href="#" title="" className="inline-flex items-center justify-center px-4 py-4 text-white transition-all duration-200 border-2 border-white rounded-md hover:bg-white hover:text-white focus:bg-black focus:text-white" role="button">
+                        <a href="#" title="" className="inline-flex items-center justify-center px-4 py-4 text-white transition-all duration-200 border-2 border-white rounded-md hover:bg-black hover:text-white focus:bg-black focus:text-white" role="button">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                             </svg>
