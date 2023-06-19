2bbde580ce31fe011640293fb8c97925073791ea
iamcollins404 <61983049+iamcollins404@users.noreply.github.com>
2023-06-19 16:51:50 +0200

    Update notFound.jsx

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import ReactGA from 'react-ga'
-import { Helmet } from "react-helmet";
+import React from 'react';
+import NavBar from '../components/landingComponents/NavBar'
+import Footer from '../components/landingComponents/Footer'
+import { Link } from "react-router-dom";
 
-function NotFound() {
-
-  React.useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  })
-
+const NotFoundPage = () => {
   return (
     <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>NRTV - Not Found</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
+      <NavBar />
+      <div className="flex items-center justify-center h-screen bg-black">
+        <div className="max-w-lg p-8 bg-gray-800 rounded-lg shadow-lg">
+          <h2 className="text-4xl text-gray-200 font-bold mb-4">Page Not Found</h2>
+          <p className="text-gray-300 mb-6">
+            Oops! The page you are looking for could not be found. Please check the URL or return to the homepage.
+          </p>
 
-      <h5 className='text-white'>
-        notFound
-      </h5>
+          <Link to="/">
+            <a className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600">
+              Return to Homepage
+            </a>
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
 
-export default NotFound
+  );
+};
+
+export default NotFoundPage;
